dd49899d926c6616f034999635e380be3ad62c59
Ali-wajdan-it <aliwajdan.it@gmail.com>
2024-09-27 19:28:03 +0500

    checking header

---
+++ src/Header.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import Sidebar from "./Sidebar"
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+    <header className="bg-slate-400 h-16">
+    <div className="h-full flex items-center justify-around">
+      <div className="font-bold text-slate-700">My Logo</div>
+      <nav className="w-2/5">
+        <ul className={`md:flex justify-around ${isOpen? 'block' : 'hidden'}`}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+      <button onClick={()=>{setIsOpen(!isOpen)}} className="h-10 w-24 bg-amber-400">Button</button>
+    </div>
+    </header>
+    <Sidebar isOpen ={isOpen}/>
+    </>
+  )
+}
+ 
---
+++ src/Sidebar.tsx
@@ -0,0 +1,20 @@
+interface gc {
+    isOpen : boolean
+}
+const Sidebar:React.FC<gc> = ({isOpen}) => {
+    console.log(isOpen)
+  return (
+    <div className={`h-96 w-44 bg-orange-300 md:block lg:hidden ${isOpen? 'block' : 'hidden'}`}>
+    <nav className={`w-2/5 relative`}>
+        <ul className={`justify-around absolute -top-12'}`}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar;
