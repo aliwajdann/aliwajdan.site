3122b282759417b5664119071765a8d6c58fd7b0
Ali Wajdan <aliwajdan.it@gmail.com>
2024-09-28 17:33:22 +0500

    changing

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
   return (
       <div className={`transition-transform duration-1000 ease-in-out flex h-full w-full lg:hidden 
       absolute top-16 left-0 z-10 ${ isOpen ? "translate-x-0" : "-translate-x-full" }`}>
-          <nav className={`w-1/3  bg-red-600`}>
+          <nav className={`w-4/5 bg-red-600`}>
            <ul className={`flex flex-col h-3/4 w-full justify-between px-4 py-10`}>
              <li className="font-bold text-white text-xl">Home</li>
              <li className="font-bold text-white text-xl">About</li>
@@ -18,9 +18,8 @@ const Sidebar = () => {
            </ul>
            <div className="h-1/4 bg-slate-600 w-full"></div>
           </nav>
-       <div  onClick={()=> dispatch(toggle())} className="h-full w-2/3"></div>
+       <div  onClick={()=> dispatch(toggle())} className="h-full w-1/5"></div>
       </div>
-
   )
 }
 
