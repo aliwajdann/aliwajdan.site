b5ad5f340565039b2d46f62150f49582cd05c0e4
Ali Wajdan <aliwajdan.it@gmail.com>
2024-09-29 11:16:42 +0500

    same as last commit

--- src/Components/Tailwindbar.tsx
+++ src/Components/Tailwindbar.tsx
@@ -4,7 +4,7 @@ import { RootState } from "../Store/Store"
 export default function Tailwindbar() {
     const isOpen = useSelector((state:RootState) => state.isOpen)
   return (
-    <div className={`absolute bg-slate-500 opacity-40 p-10 h-full w-full ${isOpen? "block" : "hidden"}`}>
+    <div className={`sm:block lg:hidden absolute bg-slate-500 opacity-40 p-10 h-full w-full ${isOpen? "block" : "hidden"}`}>
       <div className="bg-slate-700 opacity-60 h-2/3 w-full"></div>
     </div>
   )
