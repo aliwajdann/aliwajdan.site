f25d958fd37d37136adf3b360a3cf6e1d8f1f018
Ali Wajdan <aliwajdan.it@gmail.com>
2024-09-29 11:24:53 +0500

    added a little animation to tailwindbar

--- src/Components/Tailwindbar.tsx
+++ src/Components/Tailwindbar.tsx
@@ -1,10 +1,18 @@
 import { useSelector } from "react-redux"
 import { RootState } from "../Store/Store"
+// import { useRef } from "react"
 
 export default function Tailwindbar() {
     const isOpen = useSelector((state:RootState) => state.isOpen)
+    // const {divRef} = useRef()
+    // if (!isOpen){
+    //     divRef.style.transfrom.scale(0)
+    // }
+    // else{
+    //     divRef.style.transfrom.scale(100)
+    // }
   return (
-    <div className={`sm:block lg:hidden absolute bg-slate-500 opacity-40 p-10 h-full w-full ${isOpen? "block" : "hidden"}`}>
+    <div  className={`transition-transform duration-100 ease-in-ou sm:scale-100 lg:scale-0 absolute bg-slate-500 opacity-40 p-10 h-full w-full ${isOpen? "scale-100" : "scale-0"}`}>
       <div className="bg-slate-700 opacity-60 h-2/3 w-full"></div>
     </div>
   )
